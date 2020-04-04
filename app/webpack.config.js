b34bfc3ad18fbe5d148366810350255cd6432179
Daniel Portugal de Lourenço <daniportugal@gmail.com>
2020-04-04 19:30:00 -0300

    project setup

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -9,7 +9,19 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
-    new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }]),
+    new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }, { from: "./img", to: "img/" }]),
   ],
   devServer: { contentBase: path.join(__dirname, "dist"), compress: true },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [ "style-loader", "css-loader" ]
+      }
+    ],
+  },
 };
